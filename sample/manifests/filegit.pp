9edc954f33a53a2796039a26107db7575d35d70f
srikanthkodumagulla <ksc.chary@gmail.com>
2017-04-04 17:01:35 +0530

    Create filegit.pp
    Sample Git file to puppet module

---
+++ sample/manifests/filegit.pp
@@ -0,0 +1,8 @@
+file { '/home/ubuntu/srikanth.txt':
+    ensure => file,
+    #owner  => owner,
+    #group  => group,
+    #mode   => mode,
+    #source => 'puppet:///modules/class/file.txt';
+    content => 'This file is created from github editor'
+}
